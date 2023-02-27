93b737b711b2ff86c6511b6e1227ed0c680f1468
Florent <florent.pasquet@beecoming.fr>
2023-02-27 12:04:04 +0100

    contact form fix

--- src/pages/EN/contact/index.js
+++ src/pages/EN/contact/index.js
@@ -20,19 +20,11 @@ const encode = (data) => {
 
 
 const Index = (props) => {
-  const [isvalidated, setisvalidated] = useState(false);
-
-  // const iconMarkup = renderToStaticMarkup(
-  //   <img src={image} alt='localisation' className="map-icon" />
-  // );
-
-  // const customMarkerIcon = divIcon({
-  //   html: iconMarkup
-  // });
-
-  const handleChange = (e) => {
-    setisvalidated({ [e.target.name]: e.target.value })
-  }
+  const [contactForm, setContactForm] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -42,7 +34,7 @@ const Index = (props) => {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
         'form-name': form.getAttribute('name'),
-        ...isvalidated,
+        ...contactForm,
       }),
     })
       .then(() => navigate(form.getAttribute('action')))
@@ -51,88 +43,87 @@ const Index = (props) => {
   return (
     <Layout>
       <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1>Contact</h1>
-            {/* <MapContainer style={{width: "50%", height: "250px" }} center={[44.958450, 0.777649]} zoom={13} scrollWheelZoom={false}>
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        <h1>Contact</h1>
+        {/* <MapContainer style={{width: "50%", height: "250px" }} center={[44.958450, 0.777649]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[44.958450, 0.777649]} icon={customMarkerIcon}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer> */}
+        <form
+          name="contact"
+          method="post"
+          action="/EN/contact/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <div hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input
+                name="bot-field"
+                onChange={(e) => setContactForm({...contactForm})}
               />
-              <Marker position={[44.958450, 0.777649]} icon={customMarkerIcon}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-            </MapContainer> */}
-            <form
-              name="contact"
-              method="post"
-              action="/EN/contact/thanks/"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              onSubmit={handleSubmit}
-            >
-              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-              <input type="hidden" name="form-name" value="contact" />
-              <div hidden>
-                <label>
-                  Don’t fill this out:{" "}
-                  <input name="bot-field" onChange={handleChange} />
-                </label>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={"name"}>
-                  Your name
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type={"text"}
-                    name={"name"}
-                    onChange={handleChange}
-                    id={"name"}
-                    required={true}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={"email"}>
-                  Email
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type={"email"}
-                    name={"email"}
-                    onChange={handleChange}
-                    id={"email"}
-                    required={true}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={"message"}>
-                  Message
-                </label>
-                <div className="control">
-                  <textarea
-                    className="textarea"
-                    name={"message"}
-                    onChange={handleChange}
-                    id={"message"}
-                    required={true}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <button className="button is-link" type="submit">
-                  Send
-                </button>
-              </div>
-            </form>
+            </label>
           </div>
-        </div>
+          <div>
+            <label htmlFor={"name"}>
+              Your name
+            </label>
+            <div>
+              <input
+                type={"text"}
+                name={"name"}
+                value={contactForm.name}
+                onChange={(e) => setContactForm({...contactForm, name: e.target.value})}
+                id={"name"}
+                required={true}
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor={"email"}>
+              Email
+            </label>
+            <div>
+              <input
+                type={"email"}
+                name={"email"}
+                value={contactForm.email}
+                onChange={(e) => setContactForm({...contactForm, email: e.target.value})}
+                id={"email"}
+                required={true}
+              />
+            </div>
+          </div>
+          <div>
+            <label htmlFor={"message"}>
+              Message
+            </label>
+            <div>
+              <textarea
+                name={"message"}
+                value={contactForm.message}
+                onChange={(e) => setContactForm({...contactForm, message: e.target.value})}
+                id={"message"}
+                required={true}
+              />
+            </div>
+          </div>
+          <div>
+            <button className="button is-link" type="submit">
+              Send
+            </button>
+          </div>
+        </form>
       </section>
     </Layout>
   );
