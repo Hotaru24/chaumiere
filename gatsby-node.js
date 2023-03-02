d51fa3e396efab2f08ed8e09d0ac62407f573945
Florent <florent.pasquet@beecoming.fr>
2023-03-02 14:54:35 +0100

    build fix v6

--- gatsby-node.js
+++ gatsby-node.js
@@ -57,3 +57,18 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-leaflet|leaflet/,
+            use: loaders.null(),
+          }
+        ]
+      }
+    })
+  }
+};
