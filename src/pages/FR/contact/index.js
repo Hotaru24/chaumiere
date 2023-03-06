63fdd028f5021030d366514436896035588c9ebc
Florent <florent.pasquet@beecoming.fr>
2023-03-06 19:39:19 +0100

    form update try

--- src/pages/FR/contact/index.js
+++ src/pages/FR/contact/index.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { navigate } from "gatsby-link";
 import { renderToStaticMarkup } from 'react-dom/server';
 
+import Collapse from '@material-ui/core/Collapse';
+import TextField from '@mui/material/TextField';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
 import 'leaflet/dist/leaflet.css';
 import '../../contact.css';
 
@@ -11,6 +15,8 @@ import * as L from "leaflet";
 import Layout from "../../../components/Layout/Layout";
 import instagram from "../../../img/social/instagram-b.svg";
 import facebook from "../../../img/social/facebook-b.svg";
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import CallIcon from '@mui/icons-material/Call';
 import image from '../../../img/map.svg';
 
@@ -30,6 +36,10 @@ const Index = () => {
     email: '',
     message: ''
   });
+  const [formComplete, setFormComplete] = React.useState(false);
+  const [sendSuccess, setSendSuccess] = React.useState(false);
+  const [sendError, setSendError] = React.useState(false);
+  const formIsValid = contactForm.name.length > 0 && contactForm.email.length > 0 && contactForm.message.length > 0;
   
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -62,21 +72,25 @@ const Index = () => {
     e.preventDefault();
     const form = e.target;
 
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...contactForm,
-      }),
-    })
-    .then(() => navigate(form.getAttribute('action')))
-    .catch((error) => alert(error));
+    if (formIsValid) {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+          'form-name': form.getAttribute('name'),
+          ...contactForm,
+        }),
+      })
+      .then((e) => e.ok === true ? setSendSuccess(true) : setSendError(true))
+      .catch((error) => alert(error));      
+    } else {
+      setFormComplete(true);
+    }
   };
 
   return (
     <Layout>
-      <div className="page-body">
+      <div className="contact page-body">
         <section>
           <h1>Venir à la Chaumière</h1>
           <span>2630 Rte du Pécanier, 24510 Val de Louyre et Caudeau</span>
@@ -84,94 +98,190 @@ const Index = () => {
         </section>
         <section>
           <h1>Contactez nous</h1>
-          <div>
-            <span> <CallIcon /> 33 07 48 11 01 39</span>
-            <span>
+          <div className="contact-item-list">
+            <div className="contact-item">
+              <CallIcon />
+              33 07 48 11 01 39
+            </div>
+            <div className="contact-item">
               <img
                 src={facebook}
                 alt="Facebook"
-                style={{ width: "1.5rem", height: "1.5rem" }}
+                className="social-icon"
               />
               facebook
-            </span>
-            <span>
+            </div>
+            <div className="contact-item">
               <img
                 src={instagram}
                 alt="Instagram"
-                style={{ width: "1.5rem", height: "1.5rem" }}
+                className="social-icon"
               />
               instagram
-            </span>
+            </div>
           </div>
-          <form
-            name="contact"
-            method="post"
-            action="/FR/contact/thanks/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            onSubmit={handleSubmit}
-          >
-            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-            <input type="hidden" name="form-name" value="contact" />
-            <div hidden>
-              <label>
-                Don’t fill this out:{" "}
-                <input
-                  name="bot-field"
-                  onChange={(e) => setContactForm({...contactForm})}
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor={"name"}>
-                Votre nom
-              </label>
-              <div>
-                <input
-                  type={"text"}
-                  name={"name"}
-                  value={contactForm.name}
-                  onChange={(e) => setContactForm({...contactForm, name: e.target.value})}
-                  id={"name"}
-                  required={true}
-                />
+          <div className="contact-form-container">
+            <form
+              name="contact"
+              method="post"
+              action="/FR/contact/thanks/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <div hidden>
+                <label>
+                  Don’t fill this out:{" "}
+                  <input
+                    name="bot-field"
+                    onChange={() => setContactForm({...contactForm})}
+                  />
+                </label>
               </div>
-            </div>
-            <div>
-              <label htmlFor={"email"}>
-                Email
-              </label>
               <div>
-                <input
-                  type={"email"}
-                  name={"email"}
-                  value={contactForm.email}
-                  onChange={(e) => setContactForm({...contactForm, email: e.target.value})}
-                  id={"email"}
-                  required={true}
-                />
+                <label htmlFor={"name"} hidden>
+                  Nom
+                </label>
+                <div>
+                  {/* <input
+                    type={"text"}
+                    name={"name"}
+                    value={contactForm.name}
+                    onChange={(e) => setContactForm({...contactForm, name: e.target.value})}
+                    id={"name"}
+                    required={true}
+                  /> */}
+                  <TextField
+                    type={"text"}
+                    name={"name"}
+                    value={contactForm.name}
+                    onChange={(e) => setContactForm({...contactForm, name: e.target.value})}
+                    id={"name"}
+                    label="Votre nom"
+                    multiline
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <label htmlFor={"message"}>
-                Message
-              </label>
               <div>
-                <textarea
-                  name={"message"}
-                  value={contactForm.message}
-                  onChange={(e) => setContactForm({...contactForm, message: e.target.value})}
-                  id={"message"}
-                  required={true}
-                />
+                <label htmlFor={"email"} hidden>
+                  Email
+                </label>
+                <div>
+                  {/* <input
+                    type={"email"}
+                    name={"email"}
+                    value={contactForm.email}
+                    onChange={(e) => setContactForm({...contactForm, email: e.target.value})}
+                    id={"email"}
+                    required={true}
+                  /> */}
+                  <TextField
+                    type={"text"}
+                    name={"email"}
+                    value={contactForm.email}
+                    onChange={(e) => setContactForm({...contactForm, email: e.target.value})}
+                    id={"email"}
+                    label="Email"
+                    multiline
+                  />
+                </div>
               </div>
-            </div>
-            <div>
-              <button className="button is-link" type="submit">
-                Envoyer
-              </button>
-            </div>
-          </form>
+              <div>
+                <label htmlFor={"message"} hidden>
+                  Message
+                </label>
+                <div>
+                  {/* <textarea
+                    name={"message"}
+                    value={contactForm.message}
+                    onChange={(e) => setContactForm({...contactForm, message: e.target.value})}
+                    id={"message"}
+                    required={true}
+                  /> */}
+                  <TextField
+                    type={"text"}
+                    name={"message"}
+                    value={contactForm.message}
+                    onChange={(e) => setContactForm({...contactForm, message: e.target.value})}
+                    id={"message"}                    
+                    label="Message"
+                    multiline
+                    rows={6}
+                  />
+                </div>
+              </div>
+              <div>
+                <Button variant="outlined" className="button is-link" type="submit">
+                  Envoyer
+                </Button>
+              </div>
+            </form>
+            <Collapse in={sendSuccess}>
+              <Alert
+                severity="success"
+                action={
+                  (
+                    <IconButton
+                      aria-label="close"
+                      color="inherit"
+                      size="small"
+                      onClick={() => {
+                        setSendSuccess(false);
+                      }}
+                    >
+                      <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                  )
+                }
+              >
+                Message envoyé !
+              </Alert>
+            </Collapse>
+            <Collapse in={formComplete}>
+              <Alert
+                severity="error"
+                action={
+                  (
+                    <IconButton
+                      aria-label="close"
+                      color="inherit"
+                      size="small"
+                      onClick={() => {
+                        setFormComplete(false);
+                      }}
+                    >
+                      <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                  )
+                }
+              >
+                Tous les champs doivent être complétés
+              </Alert>
+            </Collapse>
+            <Collapse in={sendError}>
+              <Alert
+                severity="error"
+                action={
+                  (
+                    <IconButton
+                      aria-label="close"
+                      color="inherit"
+                      size="small"
+                      onClick={() => {
+                        setSendError(false);
+                      }}
+                    >
+                      <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                  )
+                }
+              >
+                Une erreur est survenue lors de l'envoi du message. En cas de problème n'hésitez pas à nous contacter par téléphone.
+              </Alert>
+            </Collapse>
+          </div>
         </section>
       </div>
 
