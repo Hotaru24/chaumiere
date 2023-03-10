976820e3db8dec647248f9956c57a8fb62b26f59
Florent <florent.pasquet@beecoming.fr>
2023-03-10 18:30:20 +0100

    slider zoom add

--- src/templates/rooms-post.js
+++ src/templates/rooms-post.js
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import { kebabCase } from "lodash";
-import { Helmet } from "react-helmet";
-import { graphql, Link } from "gatsby";
-import Layout from "../components/Layout/Layout";
-import Content, { HTMLContent } from "../components/Content";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import IconButton from '@mui/material/IconButton';
+import { Helmet } from "react-helmet";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+
+import './rooms.css';
+
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 import { orange } from '@mui/material/colors';
-import './rooms.css';
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+import Content, { HTMLContent } from "../components/Content";
+import Layout from "../components/Layout/Layout";
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '60vw',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 // eslint-disable-next-line
 export const RoomsPostTemplate = ({
@@ -25,7 +43,7 @@ export const RoomsPostTemplate = ({
 }) => {
   const PostContent = contentComponent || Content;
   const [locale, setLocale] = useState('FR');
-  const slideImages = [];
+  const [openModale, setOpenModale] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem('locale')) {
@@ -43,6 +61,42 @@ export const RoomsPostTemplate = ({
     slideCount > 0 ? setSlideCount(slideCount - 1) : setSlideCount(images.length - 1);
   }
 
+  const sliderTemplate = () => {
+    return (
+      <div className="rooms-slider">
+        <div
+          onClick={() => {setOpenModale(true)}}
+          style={{
+            cursor: openModale ? 'auto' : 'zoom-in',
+            maxHeight: openModale ? 'auto' : '50vh'
+          }}
+        >
+          { images &&
+            images.map((img, index) => {
+              return (
+                <GatsbyImage
+                  image={getImage(img.roomimage)}
+                  style={{ display: slideCount === index ? 'block' : 'none', maxHeight: openModale ? '80vh' : '50vh'}}
+                  alt={""}
+                  key={index}
+                />
+              )
+            })
+          }
+        </div>
+        <div className="rooms-slider-buttons">
+          <IconButton onClick={handleCountDown} aria-label="right">
+            <ArrowBackIosIcon sx={{ color: orange[500] }} fontSize="large"/>
+          </IconButton>
+          <IconButton onClick={handleCountUp} aria-label="right">
+            <ArrowForwardIosIcon sx={{ color: orange[500] }} fontSize="large"/>
+          </IconButton>
+        </div>
+      </div>
+    )
+  }
+
+
   return (
     <section>
       {helmet || ""}
@@ -51,27 +105,7 @@ export const RoomsPostTemplate = ({
           {title}
         </h1>
         <div className="header-container">
-          <div className="rooms-slider">
-            { images &&
-              images.map((img, index) => {
-                return (
-                  <GatsbyImage
-                    image={getImage(img.roomimage)}
-                    style={{display: slideCount === index ? 'block' : 'none'}}
-                    alt={""}
-                  />
-                )
-              })
-            }
-            <div className="rooms-slider-buttons">
-              <IconButton onClick={handleCountDown} aria-label="right">
-                <ArrowBackIosIcon sx={{ color: orange[500] }} fontSize="large"/>
-              </IconButton>
-              <IconButton onClick={handleCountUp} aria-label="right">
-                <ArrowForwardIosIcon sx={{ color: orange[500] }} fontSize="large"/>
-              </IconButton>
-            </div>
-          </div>
+          { sliderTemplate() }
           <div className="room-detail-price">
             <p>{ locale === 'FR' ? 'A partir de' : 'From' }</p>
             <p>{price}</p>
@@ -79,11 +113,28 @@ export const RoomsPostTemplate = ({
           </div>
         </div>
 
-        <h2>{description}</h2>
+        <h2>{ description}</h2>
         <p>
-          <PostContent content={content} />          
+          <PostContent content={content} />
         </p>
       </div>
+      <Modal
+        open={openModale}
+        onClose={ () => {setOpenModale(false)} }
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+         <Box sx={style}>
+          { sliderTemplate() }
+          <Button
+            variant="outlined"
+            startIcon={<CloseIcon />}
+            onClick={ () => {setOpenModale(false)} }
+          >
+            { locale === 'FR' ? 'Fermer' : 'Close' }
+          </Button>
+         </Box>        
+      </Modal>
     </section>
   );
 };
